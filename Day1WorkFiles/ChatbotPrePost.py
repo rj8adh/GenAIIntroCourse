1ee43d5020f6271a3253fc3c0e58249ff423fb93
Advay-T <146668925+Advay-T@users.noreply.github.com>
2024-11-27 15:33:48 -0600

    Update ChatbotPrePost.py
    Algorithm?

--- Day1WorkFiles/ChatbotPrePost.py
+++ Day1WorkFiles/ChatbotPrePost.py
@@ -1,3 +1,16 @@
+"""
+Precondition: Required Libraries are imported, variables are instantiated, and request_API is defined(explained in class)
+Postcondition: Create an infinite ai chatbot WITH HISTORY that stops when the user types end
+Algorithm: Declare a list called history and initialize it to an empty list
+           While True
+             create a variable called chat that gets user input
+             create a variable called AI_answer which is assigned to the request_API function and gets ChatGPT's response 
+                  to the the user input. (We will explain how to do this)
+              print AI_answer
+              append AI_answer to history using correct format
+"""
+
+
 from openai import OpenAI
 import os
 
@@ -15,5 +28,4 @@ def request_API(prompt, tokens: bool = True):
 
   return response.choices[0].message.content.strip()
 
-# Precondition: Required Libraries are imported, variables are instantiated, and request_API is defined(explained in class)
-# Postcondition: Create an infinite ai chatbot WITH HISTORY that stops when the user types end
+  #Code Here
