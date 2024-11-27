b2f70f221e9670e06beb25874522c99d35236c9a
Advay-T <146668925+Advay-T@users.noreply.github.com>
2024-11-27 15:21:54 -0600

    Update ChatbotCW.py

--- Day1WorkFiles/ChatbotCW.py
+++ Day1WorkFiles/ChatbotCW.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import os
 
 client = OpenAI(api_key=os.getenv("API_KEY"))
-history = []
+
 
 def request_API(prompt, tokens: bool = True):
   response = client.chat.completions.create(
@@ -17,10 +17,13 @@ def request_API(prompt, tokens: bool = True):
 
 print("Hey there, welcome to the chatgpt chatbot")
 
+history = []
+
 while True:
     chat = input()
     AI_answer = request_API([{"role": "system", "content": f"You are an AI chatbot required to help the user. Use this history to generate your response: {history}), the user has asked {chat}"}], False)
     
     # Keeping track of chat history
-    history.append({"role": "system", "content": AI_answer})
     print(AI_answer)
+    history.append({"role": "system", "content": AI_answer})
+   
